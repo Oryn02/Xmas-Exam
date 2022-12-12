ce130c46d7335a0e4b9efa96cf1d241798fb5cbb
Oryn Quinlan <G00406032@atu.ie>
2022-12-12 10:31:16 +0000

    Changes made to both classes

--- src/main/java/ie/atu/week12/Student.java
+++ src/main/java/ie/atu/week12/Student.java
@@ -2,4 +2,60 @@ package ie.atu.week12;
 
 public class Student
 {
+    private String name;
+    private int ID;
+    private int age;
+    private String address;
+
+    public Student(String name, int ID, int age, String address)
+    {
+        this.name = name;
+        this.ID = ID;
+        this.age = age;
+        this.address = address;
+    }
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student details: " +
+                "name ='" + name + '\'' +
+                ", ID =" + ID +
+                ", age =" + age +
+                ", address ='" + address;
+    }
 }
--- src/main/java/ie/atu/week12/StudentApp.java
+++ src/main/java/ie/atu/week12/StudentApp.java
@@ -1,16 +1,32 @@
 package ie.atu.week12;
 
+import java.util.Scanner;
+
 public class StudentApp
 {
     public static void main(String[] args)
     {
-        new scanner = scanner();
+
+        Scanner input = new Scanner(System.in);
+
+
         System.out.println("Please enter your name");
+        Student name = new Student();
+        name.getName(input.next());
+        System.out.println(name);
+
 
         System.out.println("Please enter your Student ID");
+        Student ID = new Student();
+
 
         System.out.println("Please enter your age");
+        Student age = new Student();
+
 
         System.out.println("Please enter your address");
+        Student address = new Student();
+
+
     }
 }
